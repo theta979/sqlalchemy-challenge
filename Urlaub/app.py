436c90ad739b5b3ab1efd93d294c6584a497d447
theta979 <calebwilliams@macbookpro.mynetworksettings.com>
2024-12-12 16:52:22 -0500

    Initial commit for Module 10 Challenge

---
+++ Urlaub/app.py
@@ -0,0 +1,32 @@
+# Import the dependencies.
+
+
+# same as the notebook but we import flask
+# 
+#  
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
